0d6cc58a2141d697e8c2f2088e53d32e63fc3574
cyclonmutation <cyclonmutation@gmail.com>
2017-09-21 14:33:05 +0900

    7.2 クラスの宣言。コンストラクタに引数指定した場合。

--- app/src/main/java/jp/techacademy/yoshie/sekiguchi/javalog/Dog.java
+++ app/src/main/java/jp/techacademy/yoshie/sekiguchi/javalog/Dog.java
@@ -7,10 +7,10 @@ class Dog {
     String name;      // 名前
     int age;    // 年齢
 
-    // コンストラクタ
-    public Dog() {
-        name = "";
-        age = 0;
+    // 引数付きコンストラクタ
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     // メソッド
